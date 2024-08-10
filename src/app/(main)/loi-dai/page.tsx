f2673f34200c988f36e29d2a9d2cef1e4750c425
LocDD <locdd@vng.com.vn>
2024-08-11 02:28:04 +0700

    fix css

--- src/app/(main)/loi-dai/page.tsx
+++ src/app/(main)/loi-dai/page.tsx
@@ -48,7 +48,7 @@ const LoiDaiLDP = () => {
                     </select>
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-[#FDD38C] bg-[#C81009] rounded-r-md">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M19 9l-7 7-7-7" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M19 9l-7 7-7-7" />
                         </svg>
                     </div>
                 </div>
